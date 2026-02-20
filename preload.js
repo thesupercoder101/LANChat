bdd35e3bbd312848bbb52925d53e9237a831ef7d
thesupercoder101 <kartik.vippagunta@gmail.com>
2026-02-20 09:31:14 -0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,5 +3,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
     minimize: () => ipcRenderer.send('minimize'),
     close: () => ipcRenderer.send('close'),
-    discoverServer: () => ipcRenderer.invoke('discover-server')
+    discoverServer: () => ipcRenderer.invoke('discover-server'),
+
+    // NEW: receive server IP from main.js
+    onServerIP: (callback) => {
+        ipcRenderer.on('server-ip', (event, ip) => {
+            callback(ip);
+        });
+    }
 });
